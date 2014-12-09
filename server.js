efc688d79efb77020ad22b7a9b8ce69493da3c43
Pedro Tavares <pedrotavares.dev@gmail.com>
2014-12-09 10:24:52 +0000

    clean up

--- server.js
+++ server.js
@@ -6,29 +6,13 @@ var express = require('express'),
 
 var app = express();
 
-var logger = require('morgan');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var errorHandler = require('errorhandler');
-
-var app = express();
-
 app.set('port', process.env.PORT || 3000);
-app.use(logger('dev'));
-app.use(methodOverride());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer());
+
 app.use(express.static(path.join(__dirname, 'www')));
 
 app.get('/', function (req, res) {
-  res.sendfile(path.join(__dirname,'www/views/index.html'));
+  res.sendFile(path.join(__dirname,'www/views/index.html'));
 });
-
-if ('development' == app.get('env')) {
-  app.use(errorHandler());
-}
 
 var server = app.listen(app.get('port'), function() {
   console.log('Server listening on port ' + server.address().port);
